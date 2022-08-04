aa4dd86d18fdd884b765d6ebb0cfae00acdec935
ryunoooosuke <75517408+ryunoooosuke@users.noreply.github.com>
2022-08-05 02:17:51 +0900

    Update product.py

--- config/settings/product.py
+++ config/settings/product.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-DEBUG = False
+DEBUG = True
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
