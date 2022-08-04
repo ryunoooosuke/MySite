a8667b8b107c13918d7bb69269aad510ec95963e
ryunoooosuke <75517408+ryunoooosuke@users.noreply.github.com>
2022-08-05 01:31:40 +0900

    Update product.py

--- config/settings/product.py
+++ config/settings/product.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_NAME'],
+        'HOST': os.environ['DB_HOST'],
         'PORT': os.environ['DB_PORT']
     }
 }
